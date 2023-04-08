3d439b6ab0f6d45ab399c6f17a591b3ee9f220f4
Ngwanatuka <Ngwanatuka@gmail.com>
2023-04-08 22:29:18 +0200

    updating git

--- 0x10-https_ssl/git_push.sh
+++ 0x10-https_ssl/git_push.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/bash
 
-# Start the SSH agent
-eval "$(ssh-agent -s)"
-
-# Add your SSH key to the agent and cache the passphrase for 1 hour
-ssh-add -t 3600 -k ~/.ssh/id_ed25519 <<< "1fjieqmi12"
-
 # Prompt user for commit message
 read -p "Enter commit message: " message
 
