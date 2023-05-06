efd94179d511dc7f34b9c91702a2618eec860582
Ngwanatuka <Ngwanatuka@gmail.com>
2023-05-06 19:31:03 +0200

    fixing code

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,23 +1,8 @@
 # Puppet class to configure Nginx with a custom response header
+
 class nginx_custom_header {
   package { 'nginx':
     ensure => installed,
-  }
-
-  file { '/var/www/html/index.html':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => 'Hello World!',
-  }
-
-  file { '/var/www/html/error404.html':
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-    content => "Ceci n'est pas une page",
   }
 
   file { '/etc/nginx/sites-available/default':
@@ -26,26 +11,20 @@ class nginx_custom_header {
     group   => 'root',
     mode    => '0644',
     content => @(END_CONFIG)
-server {
-    listen 80 default_server;
-    listen [::]:80 default_server;
+    server {
+        listen 80 default_server;
+        listen [::]:80 default_server;
 
-    root /var/www/html;
-    index index.html index.htm;
+        root /var/www/html;
+        index index.html index.htm;
 
-    server_name _;
+        server_name _;
 
-    location /redirect_me {
-        return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;
+        location / {
+            try_files $uri $uri/ =404;
+            add_header X-Served-By $hostname;
+        }
     }
-
-    error_page 404 /error404.html;
-
-    location / {
-        try_files $uri $uri/ =404;
-        add_header X-Served-By $hostname;
-    }
-}
     END_CONFIG
   }
 
