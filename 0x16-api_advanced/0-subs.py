2f4e57d71ea2a1794f7c611b8b86fe50d0af0188
ngwanatuka <ngwanatuka@gmail.com>
2023-06-07 11:03:18 +0200

    the script is now using format() str

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,7 +6,7 @@ import requests
 
 
 def number_of_subscribers(subreddit):
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': 'Custom User Agent'}
 
     response = requests.get(url, headers=headers, allow_redirects=False)
