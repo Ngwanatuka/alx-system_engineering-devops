102cce4f97e8e5fedf6eccf9a2bf08329c8d28a3
ngwanatuka <ngwanatuka@gmail.com>
2023-06-15 11:52:31 +0200

    puppet manifest fixes adjust the web stack so that it can handle 1000 at a time

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -2,11 +2,11 @@
 # and edit the ulimit
 
 exec { 'edit-file':
-    command => 'sed -i "s/15/4096" /etc/default/nginx',
-    path    => ['/usr/local/bin', '/bin']
-}
+  command => 'sed -i "s/15/4096/" /etc/default/nginx',
+  path    => '/usr/local/bin/:/bin/'
+  } ->
 
 exec { 'restart-nginx':
-    command => 'service nginx restart',
-    path    => ['/usr/bin', '/usr/sbin', '/bin', '/sbin'],
+  command => 'nginx restart',
+  path    => '/etc/init.d/'
 }
